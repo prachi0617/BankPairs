094ace8fb76380fed7b3e0ea45774de0de5c109e
prachi0716 <prachiptl1707@gmail.com>
2026-04-29 14:28:40 -0400

    changes Checking and Investment

--- src/main/java/io/zipcoder/CheckingAccount.java
+++ src/main/java/io/zipcoder/CheckingAccount.java
@@ -6,21 +6,36 @@ public class CheckingAccount extends Account {
     public CheckingAccount(Object accountHolder, Double balance, String accountNumber, boolean overdraftProtection) {
         super(accountHolder, balance, accountNumber);
         // TODO: Implement constructor
+        this.overdraftProtection = overdraftProtection;
     }
 
     public boolean getOverdraftProtection() {
         // TODO: Implement getter
-        return false;
+        return overdraftProtection;
     }
 
     public void setOverdraftProtection(boolean overdraftProtection) {
         // TODO: Implement setter
+        this.overdraftProtection = overdraftProtection;
     }
 
     @Override
     public void debit(Double amount) {
         // TODO: Implement debit method
         // If overdraftProtection is true, don't allow balance to go negative
-        // If overdraftProtection is false, allow balance to go negative
+        if (overdraftProtection) {
+            if(amount >= getBalance()) {
+                System.out.println("Not Enough Money");
+                // If overdraftProtection is false, allow balance to go negative
+
+            else { 
+                super.debit(amount);
+            }
+            }
+
+        else {
+                super.debit(amount);
+            }
+        }
     }
 }
--- src/main/java/io/zipcoder/InvestmentAccount.java
+++ src/main/java/io/zipcoder/InvestmentAccount.java
@@ -6,26 +6,34 @@ public class InvestmentAccount extends Account {
     public InvestmentAccount(Object accountHolder, Double balance, String accountNumber, Double interestRate) {
         super(accountHolder, balance, accountNumber);
         // TODO: Implement constructor
+        this.interestRate = interestRate;
     }
 
     public Double getInterestRate() {
         // TODO: Implement getter
-        return null;
+        return interestRate;
     }
 
     public void setInterestRate(Double interestRate) {
         // TODO: Implement setter
+        this.interestRate = interestRate;
     }
 
     public void applyInterest() {
         // TODO: Implement method to apply interest to the balance
+        Double interest = getBalance() * interestRate;
         // New balance = current balance + (current balance * interest rate)
+        setBalance(getBalance() + interest);
         // Note: Interest applies even to negative balances
+        getTranscation().add("Interest: $ " + interest);
     }
 
     @Override
     public void debit(Double amount) {
         // TODO: Implement debit method
-        // Investment accounts do NOT have overdraft protection - allow balance to go negative
+        super.debit(amount);
+        // Investment accounts do NOT have overdraft protection - allow balance to go
+        // negative
+
     }
 }
